a4fe66a69018bcf28a5d12cfed0ad568e2f1d9a8
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2021-05-06 14:16:43 +0900

    add section3 challenge code

---
+++ scala-fp/src/main/scala/OptionStudy.scala
@@ -0,0 +1,21 @@
+object OptionStudy {
+
+  trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case Some(s) => Some(f(s))
+      case None => None
+    }
+
+    def getOrElse[B >: A](default: => B): B = this match {
+      case Some(s) => s
+      case None => default
+    }
+
+    def flatMap[B](f: A => Option[B]): Option[B] = this map f getOrElse None
+  }
+
+  case class Some[+A](get: A) extends Option[A]
+
+  case object None extends Option[Nothing]
+
+}
