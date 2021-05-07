0bf21dc0af1173081c1b724635b53622899c721a
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2021-05-07 11:28:07 +0900

    add section8 challenge code

---
+++ scala-fp/src/main/scala/Additive.scala
@@ -0,0 +1,35 @@
+object ChallengeAdditive {
+
+  trait Additive[A] {
+    def plus(a: A, b: A): A
+
+    def zero: A
+  }
+
+  // Challenge 8-3
+  case class Polynomial(coefficient: List[Double])
+
+  object Polynomial {
+    implicit object PolynomialAdditive extends Additive[Polynomial] {
+      def plus(a: Polynomial, b: Polynomial) : Polynomial = {
+
+        def plus_internal(_a: Polynomial, _b: Polynomial): Polynomial = {
+          assert(_a.coefficient.length < _b.coefficient.length)
+          val padding_a: List[Double] = _a.coefficient ++ List.fill(_b.coefficient.length - _a.coefficient.length)(0.0)
+          // assert(padding_a.length == _b.coefficient.length)
+          Polynomial(padding_a.zip(_b.coefficient).map(n => n._1 + n._2))
+        }
+
+        if(b.coefficient.length < a.coefficient.length) {
+          plus_internal(b, a)
+        } else {
+          plus_internal(a, b)
+        }
+      }
+
+      def zero: Polynomial = Polynomial(List(0))
+    }
+  }
+
+  def sum[A](lst: List[A])(implicit m: Additive[A]): A = lst.foldLeft(m.zero)((x, y) => m.plus(x, y))
+}
